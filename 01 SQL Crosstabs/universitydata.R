fe1ae71e9696ee369afffd1cce35bfef7134ede2
Rhoda Jiao <r.jiao@utexas.edu>
2015-04-15 11:30:26 -0500

    text on html

--- 01 SQL Crosstabs/universitydata.R
+++ 01 SQL Crosstabs/universitydata.R
@@ -1,5 +1,6 @@
 universitydf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="select * from UNIVERSITYDATA "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 options(dplyr.width = Inf)
+print("States Ranked by Entering Full Time University Students in 2013")
 rankdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select STATE, EnrollTotals,rank() over(order by EnrollTotals desc) as Rank from
 (
@@ -9,6 +10,7 @@ group by STATE
 )"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 tbl_df(rankdf)
 
+print("Maximum Total Entering Class of 2013 For Each University System in Texas")
 maxdiffdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select STATE, SYSTEMNAME, NAME, ENTERINGCLASS2013, last_value(max_enteringclass) 
 OVER (PARTITION BY SYSTEMNAME order by ENTERINGCLASS2013 desc) max_enteringclass, last_value(max_enteringclass) 
@@ -22,6 +24,7 @@ order by 2,4 desc"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 tbl_df(maxdiffdf)
 
+print("Largest Full Time Entering Class in 2013 by State")
 nthdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT state, city, name, fulltimeentering2013, nth_value(fulltimeentering2013,1)
 OVER (PARTITION BY state ORDER BY fulltimeentering2013 desc) max_entering
@@ -30,6 +33,7 @@ WHERE fulltimeentering2013 <> 0
 ORDER BY 1,2,3"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 tbl_df(nthdf)
 
+print("Cumulative Percentage of Full Time Entering Class Size in 2013 by State")
 cumedistdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT state, city, name, fulltimeentering2013, cume_dist()
 OVER (PARTITION BY state ORDER BY fulltimeentering2013) cume_dist
