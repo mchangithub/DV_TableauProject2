084ccc16e0ccd8b0fe1c21778bb89d8418a37e0e
maytee <mayteechan@utexas.edu>
2015-04-14 21:55:00 -0500

    Finishing up

--- 01 SQL Crosstabs/universitydata.R
+++ 01 SQL Crosstabs/universitydata.R
@@ -1,5 +1,5 @@
 universitydf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="select * from UNIVERSITYDATA "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-
+options(dplyr.width = Inf)
 rankdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select STATE, EnrollTotals,rank() over(order by EnrollTotals desc) as Rank from
 (
@@ -7,7 +7,7 @@ select STATE, sum(FULLTIMEENTERING2013) as EnrollTotals
 from UNIVERSITYDATA
 group by STATE
 )"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
-head(rankdf)
+tbl_df(rankdf)
 
 maxdiffdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select STATE, SYSTEMNAME, NAME, ENTERINGCLASS2013, last_value(max_enteringclass) 
@@ -20,7 +20,7 @@ FROM UNIVERSITYDATA)
 where SYSTEMNAME <> \\\' \\\' and ENTERINGCLASS2013 is not null and ENTERINGCLASS2013>0 and STATE = \\\'TX\\\'
 order by 2,4 desc"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
-head(maxdiffdf)
+tbl_df(maxdiffdf)
 
 nthdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT state, city, name, fulltimeentering2013, nth_value(fulltimeentering2013,1)
@@ -28,7 +28,7 @@ OVER (PARTITION BY state ORDER BY fulltimeentering2013 desc) max_entering
 FROM universitydata
 WHERE fulltimeentering2013 <> 0
 ORDER BY 1,2,3"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-head(nthdf)
+tbl_df(nthdf)
 
 cumedistdf <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT state, city, name, fulltimeentering2013, cume_dist()
@@ -36,4 +36,4 @@ OVER (PARTITION BY state ORDER BY fulltimeentering2013) cume_dist
 FROM universitydata
 WHERE fulltimeentering2013 <> 0
 ORDER BY 1,2,3"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mc44693', PASS='orcl_mc44693', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-head(cumedistdf)
+tbl_df(cumedistdf)
